e7c2e106a835e766d4c37b76960f620ce6c1b2ea
Alexander Kit <source.kit@gmail.com>
2013-10-07 13:49:53 +0200

    [++] readmes

---
+++ tools/readme.js
@@ -0,0 +1,26 @@
+
+var SOURCE = '../',
+	TARGET = 'public/view/'
+
+include.exports = {
+	
+	process: function(){
+		var readmes = {
+			'node/io/': 'atma-io/',
+			'node/logger/': 'atma-logger/',
+			'node/server/': 'atma-server/',
+			'utest/' : 'utest/'
+		};
+		
+		
+		for(var key in readmes){
+			
+			var source = net.Uri.combine(SOURCE, key, 'README.md'),
+				target = net.Uri.combine(TARGET, readmes[key], 'README.md');
+				
+			new io.File(source).copyTo(target);
+		}
+		
+	}
+	
+};
